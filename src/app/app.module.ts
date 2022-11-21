aa45abebc6d565fd102a058ba954267bf0030d69
Markus Querman <65109364+MQrmn@users.noreply.github.com>
2022-11-21 21:26:40 +0500

    Added material-tree

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,15 +4,36 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+
+import {MatTreeModule} from '@angular/material/tree';
+import {TreeChecklistExample} from './components/material-tree/tree-checklist-example';
+
+import {FormsModule, ReactiveFormsModule} from '@angular/forms';
+import {MatCheckboxModule} from '@angular/material/checkbox';
+import {MatIconModule} from '@angular/material/icon';
+import {MatFormFieldModule} from '@angular/material/form-field';
+
+// import {TreeChecklistExample} from './components/material-tree/tree-checklist-example';
+
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TreeChecklistExample
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    MatTreeModule,
+    FormsModule, 
+    ReactiveFormsModule,
+    MatCheckboxModule,
+    MatIconModule,
+    MatFormFieldModule
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [
+    AppComponent,
+    TreeChecklistExample
+  ]
 })
 export class AppModule { }
